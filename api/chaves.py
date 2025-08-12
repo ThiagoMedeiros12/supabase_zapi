40b05ef739fab686ee03ea31f713eb5b17bfd16d
Thiago Ribeiro do Val Medeiros <thia.med@hotmail.com>
2025-08-12 14:55:37 -0300

    Delete api/chaves.py

--- api/chaves.py
+++
@@ -1,3 +0,0 @@
-SUPABASE_URL="https://dkoawgndschrvbqojibl.supabase.co"
-KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrb2F3Z25kc2NocnZicW9qaWJsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTAxMjYzMywiZXhwIjoyMDcwNTg4NjMzfQ.3cLTnNT5KIrUSGGwTIKG-dkNQN-4NWJxftiGVtIF3vY"
-
