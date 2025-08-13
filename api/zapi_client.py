e11fd494584b92d3dfcf62c099f5b119901e2a4a
Thiago Medeiros <thia.med@hotmail.com>
2025-08-12 22:54:28 -0300

    Erro na hora de enviar a msg sem o token de cliente

--- api/zapi_client.py
+++ api/zapi_client.py
@@ -1,16 +1,23 @@
 import requests
-from api.chaves import  TOKEN_ZAPI ,ID_ZAPI
+from api.chaves import  TOKEN_ZAPI ,ID_ZAPI,CLIENT_TOKEN
 
 def send_mensagem(phone_number, name):
     instance = ID_ZAPI
+    client_token = CLIENT_TOKEN
     token = TOKEN_ZAPI
     url = f"https://api.z-api.io/instances/{instance}/token/{token}/send-text"
-    payload = {
+   
+    headers = {
+        "Content-Type": "application/json",
+        "client-token": client_token
+        }
+    payload = {    
         "phone": phone_number,
         "message": f"Olá {name}, tudo bem com você?"        
         }
+    
     try:
-        response = requests.post(url, json=payload)
+        response = requests.post(url,headers=headers,json=payload)
         response.raise_for_status()
         print(f"Mensagem enviada para {name} ({phone_number})")
     except requests.RequestException as e:
